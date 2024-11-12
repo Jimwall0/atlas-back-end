0c3b2d841b9c55c3761503fd48d4082f3f100d3e
Tsimmuaj <tyang4204@gmail.com>
2024-11-12 07:45:49 -0600

    working output

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,21 +11,31 @@ def fetch_request(url):
 
 
 def todo_list(employee_id):
-    employee = "https://jsonplaceholder.typicode.com/users/{employee_id}"
-    employee_todos = "https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    employee = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    employee_todos = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
     try:
         todos = fetch_request(employee_todos)
-        todos_list = [todos["title"] for item in todos if todos["completed"]]
+        todos_list = [item["title"] for item in todos if item["completed"]]
         todos_len = len(todos)
         todos_list_len = len(todos_list)
         employee_data = fetch_request(employee)
         user_name = employee_data["name"]
         print(f"Employee {user_name} is done with tasks({todos_list_len}/{todos_len}):")
-        for n in todos_len:
-            print(todos["title"])
-    except:
-        print("Error with code")
+        for n in todos_list:
+            print(f"\t {n}")
+    except urllib.error.URLError as e:
+        print(e)
 
-if __name__ == "__main__":
-    index()
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Please provide a valid integer for the employee ID.")
+        sys.exit(1)
+
+    todo_list(employee_id)
